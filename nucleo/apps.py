3fd65e3562a313ecee06ff8c8ac09d7183e2e9cf
DokiTron56 <pipe.silvar2006@gmail.com>
2026-02-10 13:46:08 -0300

    fix ready init admin

--- nucleo/apps.py
+++ nucleo/apps.py
@@ -1,9 +1,7 @@
 from django.apps import AppConfig
 
-
 class NucleoConfig(AppConfig):
     name = 'nucleo'
 
-def ready(self):
-    import config.init_admin
-
+    def ready(self):
+        import config.init_admin
